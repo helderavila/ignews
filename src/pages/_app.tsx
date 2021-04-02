e28c207a224ace081bc8fd6142975218f2dd6170
Helder Avila <helderavila2016@gmail.com>
2021-04-02 14:45:39 -0300

    feat: Autenticação com Github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from 'next/app'
 
+import { Provider as NextAuthProvider } from 'next-auth/client'
+
 // Components
 import { Header } from '../components/Header'
 
@@ -7,10 +9,10 @@ import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   )
 }
 
